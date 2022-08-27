87bc6702cb4777aa5ec37fc36d8e8522f0bce4a4
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2022-08-27 10:01:46 +0100

    Basic game state with a start but not a stop

--- astrarisk.js
+++ astrarisk.js
@@ -1,38 +1,46 @@
-// Obtain the canvas dimensions
-var elem = document.getElementById( 'game' )
-var width = elem.offsetWidth;
-var height = elem.offsetHeight;
-elem.width = width
-elem.height = height
-
-// Global graphics context for the canvas
-var gfx = elem.getContext( '2d' )
-var lastRender = 0
+// ==============================================================================================
+// Game state stuff!
+// We need to know the canvas dimensions to calculate some of this ...
+// ==============================================================================================
+var game = document.getElementById( 'game' )
+var width = game.offsetWidth;
+var height = game.offsetHeight;
 
 // Simple object to track the beam's position
 var state = {
     x: 0,
-    y: height / 2
+    y: 0
 }
 
-// =======================
 // Moves the beam
-// =======================
+// =====================
 function beam( delta ) {
+    // Move right
     state.x += delta/2
     if ( state.x > width ) {
         state.x = 0
         state.y = height/2
     }
     
-    state.y += delta/2
+    // Move up or down depending on mousey
+    if ( isPressing ) {
+        state.y -= delta/2
+    } else {
+        state.y += delta/2
+    }
 }
 
-// =======================
+// ==============================================================================================
+// Graphics and painting stuff!
+// Global graphics context for the canvas
+// ==============================================================================================
+var gfx = game.getContext( '2d' )
+
 // Draws the beam
-// =======================
+// ===========================
 function paint() {
-    gfx.clearRect(0, 0, width, height)
+    gfx.clearRect( 0, 0, width, height );
+    gfx.beginPath();
     gfx.strokeStyle = "#40d060"
     gfx.lineWidth = 5
     gfx.moveTo( 0, height/2 )
@@ -40,8 +48,14 @@ function paint() {
     gfx.stroke()
 }
 
-// =======================
-// Game loop.
+// ==============================================================================================
+// Game loop stuff!
+// Tracks the time difference between frame updates.
+// ==============================================================================================
+var lastRender = 0
+
+// Main game loop function. 
+// Very simple: work out time diff, update everything, paint everything, repeat until death
 // =======================
 function loop( now ) {
     // Move the beam along and repaint the canvas
@@ -52,4 +66,45 @@ function loop( now ) {
     window.requestAnimationFrame( loop )
 }
 
-//window.requestAnimationFrame( loop )
+// Start the game!
+// ===============
+function play() {
+    // Make the canvas visible
+    game.style.display = 'block';
+    window.requestAnimationFrame( initDisplay )    
+}
+
+// Initialise the game display
+// ===========================
+function initDisplay( timestamp ) {
+    // update the dimensions now they're known
+    width = game.offsetWidth;
+    height = game.offsetHeight;
+
+    game.width = width
+    game.height = height
+    state.x = 0
+    state.y = height / 2
+
+    lastRender = timestamp
+    window.requestAnimationFrame( loop )    
+}
+
+// ==============================================================================================
+// Mouse stuff!
+// Listen for mouse up and mouse down (hopefully fingers on touch devices!)
+// ==============================================================================================
+
+// When true, someone is pressing the mouse/touching the screen
+var pressing = false;
+
+// Determines if pressing is occurring or not. 
+// ==============================
+function isPressing( e ) {
+    var flags = e.buttons !== undefined ? e.buttons : e.which;
+    isPressing = (flags & 1) === 1;
+}
+
+// Update state when the mouse buttons are pressed or released.
+document.addEventListener( "mousedown", isPressing );
+document.addEventListener( "mouseup", isPressing );
